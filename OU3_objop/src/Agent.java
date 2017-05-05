ae0bceca6f25c8aeed662785a19bde0a7384a106
Marko Nygard <oi12mnd@cs.umu.se>
2017-05-05 10:33:13 +0200

    add Agent v1

--- OU3_objop/src/Agent.java
+++ OU3_objop/src/Agent.java
@@ -1,11 +1,47 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+
 /**
  * Created by oi12mnd on 2017-05-05.
  */
 public class Agent {
 
-    private
-    public Agent(Node startNode, int maxJumps){
+    private HashMap routingTable = new HashMap<Integer, ArrayList<Integer>>();
+    private Node currNode, nextNode;
+    private int jumps;
+    private ArrayList<Node> visitedNodes = new ArrayList<Node>(); // index 0 - distance, 1 - direction.
 
+    /** Sets the maximum number of jumps of an agent. Adds the starting node to visited nodes.
+     * @param startNode The node where the agent is created.
+     * @param maxJumps The number of */
+    public Agent(Node startNode, int maxJumps){
+        final int MAXJUMPS = maxJumps;
+        visitedNodes.add(startNode);
     }
 
+    /* Check which nodes are valid moves */
+    private ArrayList<Node> getMovableNeighbours(){
+        ArrayList<Node> movableNeighbours = new ArrayList<Node>();
+        for (Node node: currNode.getNeighbours()){
+            if (!visitedNodes.contains(node)){
+                movableNeighbours.add(node);
+            }
+        }
+        return movableNeighbours;
+    }
+
+    /* Pick a random node from the current node's neighbours. */
+    private Node getNextNode(ArrayList<Node> movableNeighbours){
+        int nextNodeIndex = (int) Math.random()*movableNeighbours.size();
+    }
+
+    /** Moves the agent one step. */
+    public void move(){
+        jumps++;
+    }
+
+    /** Synchronises the agent's information with a node. */
+    public void update(){
+
+    }
 }
