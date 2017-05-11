991a3904443685c364d45f985fe6cd21417bf69f
wallgren <wallgren.gustav@gmail.com>
2017-05-11 11:56:40 +0200

    beenHere in reqeust

--- OU3_objop/src/Request.java
+++ OU3_objop/src/Request.java
@@ -46,18 +46,21 @@ public class Request extends Message{
      */
     @Override
     public Node findNextNode(){
+        boolean beenHere = false;
         if(message == null ){
             if(currNode.getEventInfo(eventId) != null){
                 idFound = true;
                 directionNext = currNode.getEventInfo(eventId).get(1);
+                beenHere = true;
 
             }
             if(!idFound){
                 return currNode.getNeighbours().get(random.nextInt(currNode.getNeighbours().size()));
             }
-            else{
+            else if(beenHere){
                 return currNode.getNeighbours().get(directionNext);
             }
+            return null;
         }
         else{
             System.out.println("Is on way back");
