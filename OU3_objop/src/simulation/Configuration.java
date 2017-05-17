ba70090c7c1bd53a08b7dfe8655153e56d66c6ec
Ludvig Paju <c16lpu@cs.umu.se>
2017-05-17 17:06:43 +0200

    Fixed comments

--- OU3_objop/src/simulation/Configuration.java
+++ OU3_objop/src/simulation/Configuration.java
@@ -3,8 +3,8 @@ package simulation;
 import java.util.ArrayList;
 
 /**
- * Created by Max Holmberg on 2017-05-12.
- * Class - Configuration class to set the parameters to grid
+ * Class - Configuration class to set the parameters to grid.
+ * Created by grupp 8 on 2017-05-12.
  */
 public class Configuration {
 
@@ -17,13 +17,13 @@ public class Configuration {
     private int timeEachRequestsIsSent;
 
     /**
-     * Constructor - Creates a Configuration class
+     * Constructor - Creates a Configuration class.
      */
     public Configuration() {}
 
     /**
-     * Method - Sets the Nodes to the given node list
-     * @param nodes
+     * Method - Sets the Nodes to the given node list.
+     * @param nodes: The nodes to add to the system.
      */
     public void setNodes(ArrayList<Node> nodes) {
         if (nodes.size() < 4)
@@ -32,84 +32,84 @@ public class Configuration {
     }
 
     /**
-     * Method - Sets the maxJumpsRequest to the given parameter
-     * @param maxJumpsRequest
+     * Method - Sets the maxJumpsRequest to the given parameter.
+     * @param maxJumpsRequest: The maximum number of jumps that a request can perform.
      */
     public void setMaxJumpsRequest(int maxJumpsRequest) {this.maxJumpsRequest = maxJumpsRequest;}
 
     /**
-     * Method - Sets the maxJumpsAgents to the given parameter
-     * @param maxJumpsAgent
+     * Method - Sets the maxJumpsAgents to the given parameter.
+     * @param maxJumpsAgent: The maximum number of jumps that an agent can perform.
      */
     public void setMaxJumpsAgent(int maxJumpsAgent) {this.maxJumpsAgent = maxJumpsAgent;}
 
     /**
-     * Method - Sets the comLength to the given parameter
-     * @param comlength
+     * Method - Sets the comLength to the given parameter.
+     * @param comlength: The radius within which a node can detect other nodes.
      */
     public void setComlength(int comlength) {this.comlength = comlength;}
 
     /**
-     * Method - Sets the eventProbability to the given parameter
-     * @param eventProbability
+     * Method - Sets the eventProbability to the given parameter.
+     * @param eventProbability: is what the probability will be for an event to be created.
      */
     public void setEventProbability(double eventProbability) {this.eventProbability = eventProbability;}
 
     /**
-     * Method - Sets the agentProbability to the given paramter
-     * @param agentProbability
+     * Method - Sets the agentProbability to the given paramter.
+     * @param agentProbability: is what the probability will be for an agent to be created.
      */
     public void setAgentProbability(double agentProbability) {this.agentProbability = agentProbability;}
 
     /**
-     * Method - Sets the timeEachRequestisSent
-     * @param timeEachRequestsIsSent
+     * Method - Sets the timeEachRequestisSent.
+     * @param timeEachRequestsIsSent: is how often the program will send requests.
      */
     public void setTimeEachRequestsIsSent(int timeEachRequestsIsSent){
         this.timeEachRequestsIsSent = timeEachRequestsIsSent;
     }
 
     /**
-     * Method - Gets the list of nodes
-     * @return ArrayList<Node>
+     * Method - Gets the list of nodes.
+     * @return ArrayList<Node>: List of the nodes in the system.
      */
     public ArrayList<Node> getNodes() {
         return nodes;
     }
 
     /**
-     * Method - Gets the maxJumpsRequest
-     * @return int
+     * Method - Gets the maxJumpsRequest.
+     * @return int: The maximum number of jumps that a request can perform.
      */
     public int getMaxJumpsRequest() {return maxJumpsRequest;}
 
     /**
-     * Method - Gets the maxJumpsAgent
-     * @return int
+     * Method - Gets the maxJumpsAgent.
+     * @return int: The maximum number of jumps that a agent can perform.
      */
     public int getMaxJumpsAgent() {return maxJumpsAgent;}
 
     /**
-     * Method - Gets the comLength
-     * @return int
+     * Method - Gets the comLength.
+     * @return int: The radius within which a node can detect other nodes.
      */
     public int getComlength() {return comlength;}
 
     /**
-     * Method - Gets the eventProbability
-     * @return double
+     * Method - Gets the eventProbability.
+     * @return double: The probability that a event is created.
      */
     public double getEventProbability() {return eventProbability;}
 
     /**
-     * Method - Gets the agentProbability
-     * @return double
+     * Method - Gets the agentProbability.
+     * @return double: The probability that a agent is created.
      */
     public double getAgentProbability() {return agentProbability;}
 
     /**
-     * Method - Gets the timeEachRequestIsSent
-     * @return int
+     * Method - Gets the timeEachRequestIsSent.
+     * @return int: How often a request is created.
      */
     public int getTimeEachRequestsIsSent() {return timeEachRequestsIsSent; }
 }
--- OU3_objop/src/simulation/Request.java
+++ OU3_objop/src/simulation/Request.java
@@ -1,7 +1,8 @@
 package simulation;
 
 /**
- * Class - Request class
+ * Class - Request class that simulates a request message.
+ * Created by grupp 8 on 2017-05-05
  */
 public class Request extends Message{
     private int eventId;
@@ -9,10 +10,10 @@ public class Request extends Message{
     private int directionNext;
 
     /**
-     * Constructor        - Creates a Request from a given node
-     * @param requestFrom
-     * @param eventId
-     * @param MAXJUMPS
+     * Constructor - Creates a Request from a given node.
+     * @param requestFrom: Is the node where the request was sent from.
+     * @param eventId: Is the ID of the event that a request is looking for.
+     * @param MAXJUMPS: Is the maximum amount of jumps that a request can perform.
      */
     public Request(Node requestFrom, int eventId, int MAXJUMPS){
         super(requestFrom, MAXJUMPS);
@@ -20,14 +21,14 @@ public class Request extends Message{
     }
 
     /**
-     * Method  - Returns the message of this Request
-     * @return String message
+     * Method - Returns the message of this Request.
+     * @return The string message.
      */
     public String getMessage(){ return message; }
 
 
     /**
-     * Method - Updates the Request
+     * Method - Updates the Request.
      */
     public void update(){
         if(currNode.returnTimeIfEventExists(eventId) != null){
@@ -42,8 +43,8 @@ public class Request extends Message{
     }
 
     /**
-     * Method - Finds the next node for the Request to move to
-     * @return Node
+     * Method - Finds the next node for the Request to move to.
+     * @return The next node for a request to move to.
      */
     @Override
     protected Node findNextNode(){
@@ -64,16 +65,16 @@ public class Request extends Message{
     }
 
     /**
-     * Method - Returns eventId
-     * @return int
+     * Method - Returns eventId.
+     * @return int: The ID of the event.
      */
     public int getEventId(){
         return eventId;
     }
 
     /**
-     * Method - Returns MAXJUMPS of the Request
-     * @return int
+     * Method - Returns MAXJUMPS of the Request.
+     * @return int: The amount of jumps that a request can perform.
      */
     public int getMaxJumps(){
         return MAXJUMPS;
@@ -81,8 +82,8 @@ public class Request extends Message{
 
     /**
      * Method  - Returns true if the Request have returned
-     *           to the Node where Request was initialized
-     * @return boolean
+     *           to the Node where Request was initialized.
+     * @return boolean: True if a request has returned, otherwise false.
      */
     public boolean hasReturned(){
         return path.isEmpty();
